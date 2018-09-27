7a9a742cf0f9f4f7c5ca0c30bf2a2dc22d19b2e4
github@plunge.de <root@smarthome.sthome.de>
2018-09-27 11:58:53 +0200

    Autocommit

--- FTS14EM/module.php
+++ FTS14EM/module.php
@@ -7,7 +7,8 @@
 	  // erzeugt benötigte variablen etc.
 	  $this->RegisterPropertyInteger("DeviceID", 0);
 	  	
-	  
+   	  $this->RequireParent("{52FEFE9-7858-4B8E-A96E-26E15CB944F7}");
+
       	  $this->RegisterVariableBoolean("Switch0","Switch0");
       	  $this->RegisterVariableBoolean("Switch1","Switch1");
       	  $this->RegisterVariableBoolean("Switch2","Switch2");
