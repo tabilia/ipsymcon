ed44aa5a34d9f18a90a554bbc3728af2483280e0
github@plunge.de <root@smarthome.sthome.de>
2018-09-27 15:43:58 +0200

    Autocommit

--- FTS14EM/module.php
+++ FTS14EM/module.php
@@ -30,19 +30,19 @@
 	public function ApplyChanges()
 	{
 	  parent::ApplyChanges();
-	
+	  $this->SetReceiveDataFilter("A5 5A 0B 05.*");	
 	  $this->SendDebug("FTS14",$this->GetConfigurationForParent(),0);
 	}
+
+
 
 	
 	public function ReceiveData($JSONString) {
 		// Empfangene Daten vom I/O
-		echo "ok";
 
 		$data = json_decode($JSONString);
 		$this->LogMessage("ReceiveData aufgerufen", KL_DEBUG);
 		IPS_LogMessage("FTS14EM-ReceiveData", utf8_decode($data->Buffer));
-		SetValue($this->GetIDForIdent("Switch0"), true);
 		$this->SendDebug("ReceiveData",utf8_decode($data->Buffer),0);
 		
 
@@ -55,3 +55,39 @@
 
   }
 ?>
+
+/* Beispieltelegram:
+ *
+ * A5 5A 0B 05  00 	00 	00 	00 	00 	00 	10 	01 	20 41 
+ * x  x  x  ORG byte3   byte2	byte1	byte0	send3	send2	send1	send0	
+ *
+ *
+FTS14EM (nur Telegramme für den Eltako-RS485-Bus)
+Je nach eingestelltem ID- Bereich (Addition aus unterem Drehschalter +
+oberem Drehschalter + 1000) ergeben sich folgende Basis- ID´s.
+Beispiel für Gruppe 1: 1 (unterer Drehschalter) +0 (oberer Drehschalter)
++1000 = Basis- ID = 1001
+Beispiel für Gruppe 1: 1 (unterer Drehschalter) +90 (oberer Drehschalter)
++1000 = Basis- ID = 1091
+Beispiel für Gruppe 5: 401 (unterer Drehschalter) +30 (oberer Drehschalter)
++1000 = Basis- ID = 1431
+ORG = 0x05
+Einstellung UT
+Data_byte3 = Ansteuerung von +E1 -> 0x70 (Basis-ID +0)
+ Ansteuerung von +E2 -> 0x50 (Basis-ID +1)
+ Ansteuerung von +E3 -> 0x30 (Basis-ID +2)
+ Ansteuerung von +E4 -> 0x10 (Basis-ID +3)
+ Ansteuerung von +E5 -> 0x70 (Basis-ID +4)
+ Ansteuerung von +E6 -> 0x50 (Basis-ID +5)
+ Ansteuerung von +E7 -> 0x30 (Basis-ID +6)
+ Ansteuerung von +E8 -> 0x10 (Basis-ID +7)
+ Ansteuerung von +E9 -> 0x70 (Basis-ID +8)
+ Ansteuerung von +E10 -> 0x50 (Basis-ID +9)
+Bei der Einstellung RT werden automatisch Paare mit geraden ID´s gebildet:
++E1/+E2, +E3/+E4, +E5/+E6, +E7/+E8, +E9/+E10
+Wird die Ansteuerung eines Steuereingangs beendet,
+wird ein Telegramm mit der jeweiligen ID und Data_byte3 = 0x00 erzeugt.
+Data_byte2 = not used (0x00)
+Data_byte1 = not used (0x00)
+Data_byte0 = not used (0x00)
+*/
