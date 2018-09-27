a3efa56d4412e0a3e6b8e44f69688b699afb72cb
github@plunge.de <root@smarthome.sthome.de>
2018-09-27 16:09:08 +0200

    Autocommit

--- FTS14EM/module.php
+++ FTS14EM/module.php
@@ -34,7 +34,7 @@
 	  parent::ApplyChanges();
 //	  $this->SetReceiveDataFilter(".*");	
 	  $this->ConnectParent("{AC6C6E74-C797-40B3-BA82-F135D941D1A2}");
-	  $this->SendDebug("FTS14",$this->GetConfigurationForParent(),0);
+	  $this->SendDebug("FTS14","applaychanges".$this->GetConfigurationForParent(),0);
 	}
 
 
