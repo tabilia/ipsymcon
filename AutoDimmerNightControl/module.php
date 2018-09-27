92574fff3847a69d02651ce56170f963636856d1
github@plunge.de <root@smarthome.sthome.de>
2018-09-27 22:01:21 +0200

    Autocommit

---
+++ AutoDimmerNightControl/module.php
@@ -0,0 +1,26 @@
+<?
+  class AutoDimmerNightControl extends IPSModule
+  {
+	public function Create()
+	{
+	  parent::Create();
+	  // erzeugt benötigte variablen etc.
+#	  $this->RegisterPropertyInteger("UpperRotarySwitch", 0);
+#	  $this->RegisterPropertyInteger("LowerRotarySwitch", 0);
+#	  $this->RegisterPropertyInteger("SwitchType", 0);
+#	  $this->RegisterPropertyInteger("PropertyInstanceID", 0);
+	  
+
+#      	  $this->RegisterVariableBoolean("Switch0","Switch0");
+
+	}
+
+
+	
+	public function ApplyChanges()
+	{
+		parent::ApplyChanges();
+	}
+
+  }
+	
