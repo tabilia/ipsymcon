9983d1c05123bc1ed8388263a51ab6d334688be0
github@plunge.de <root@smarthome.sthome.de>
2018-09-27 22:00:24 +0200

    Autocommit

--- DimmerNightControl/module.php
+++ DimmerNightControl/module.php
@@ -1,17 +1,17 @@
 <?
-  class FTS14EM extends IPSModule
+  class DimmerNightControl extends IPSModule
   {
 	public function Create()
 	{
 	  parent::Create();
 	  // erzeugt benötigte variablen etc.
-	  $this->RegisterPropertyInteger("UpperRotarySwitch", 0);
-	  $this->RegisterPropertyInteger("LowerRotarySwitch", 0);
-	  $this->RegisterPropertyInteger("SwitchType", 0);
-	  $this->RegisterPropertyInteger("PropertyInstanceID", 0);
+#	  $this->RegisterPropertyInteger("UpperRotarySwitch", 0);
+#	  $this->RegisterPropertyInteger("LowerRotarySwitch", 0);
+#	  $this->RegisterPropertyInteger("SwitchType", 0);
+#	  $this->RegisterPropertyInteger("PropertyInstanceID", 0);
 	  
 
-      	  $this->RegisterVariableBoolean("Switch0","Switch0");
+#      	  $this->RegisterVariableBoolean("Switch0","Switch0");
 
 	}
 
