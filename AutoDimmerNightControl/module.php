4beed33eff5fc2241fa6ccf17a9837644b974967
github@plunge.de <root@smarthome.sthome.de>
2018-09-28 21:56:10 +0200

    Autocommit

--- AutoDimmerNightControl/module.php
+++ AutoDimmerNightControl/module.php
@@ -56,7 +56,7 @@
 		  $brightness=$this->ReadPropertyInteger("DimmerValueDay");
 		}
 		ENO_DimSet($DimmerID, $brightness);
-		$seconds=5;
+		$seconds=ReadPropertyInteger("MotionDetectorTimer");
 		$this->SetTimerInterval("OffTimer", $seconds * 1000);
 		$this->SendDebug("MotionDetector","Start Light",0);
 	}
@@ -65,7 +65,7 @@
 		$DimmerID=$this->ReadPropertyInteger("DimmerID");
 		$value=100;
 		ENO_DimSet($DimmerID, 100);
-		$seconds=5;
+		$seconds=ReadPropertyInteger("SwitchTimer");
 		$this->SetTimerInterval("OffTimer", $seconds * 1000);
 		$this->SendDebug("Switch","Start Light",0);
 	}
