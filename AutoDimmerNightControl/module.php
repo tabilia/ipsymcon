734b9168f798fb3f6fcb4235dad360ff455d9653
github@plunge.de <root@smarthome.sthome.de>
2018-09-29 12:32:13 +0200

    Autocommit

--- AutoDimmerNightControl/module.php
+++ AutoDimmerNightControl/module.php
@@ -103,6 +103,7 @@ class AutoDimmerNightControl extends IPSModule
 		} else {
 		// Licht ist bereits an wegen gedrücktem Switch. 
 			// TODO: entscheiden was zu tun ist
+		  $seconds=$this->ReadPropertyInteger("MotionDetectorTimer");
 		  $this->SetTimerInterval("OffTimer", $seconds * 1000);
 		  $this->SendDebug("MotionDetector","Renew Timer for ".$seconds."s",0);
 		}	
